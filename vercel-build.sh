3a5dd78255d012ea1800fc48099bb61f440f34cb
shreyas30016 <shreyasvsawant301@gmail.com>
2026-07-16 22:14:05 +0530

    Add Vercel build script to generate .env from environment variables

---
+++ vercel-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "GEMINI_API_KEY=$GEMINI_API_KEY" > .env
+echo "APP_URL=$APP_URL" >> .env
+echo "OPENWEATHER_API_KEY=$OPENWEATHER_API_KEY" >> .env
+echo "SUPABASE_URL=$SUPABASE_URL" >> .env
+echo "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" >> .env
+echo "GOOGLE_WEB_CLIENT_ID=$GOOGLE_WEB_CLIENT_ID" >> .env
+
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
+./flutter/bin/flutter build web --release
+
